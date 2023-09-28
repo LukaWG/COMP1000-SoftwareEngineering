1a674847431ee34861a210861a9fd4ac93a3c7c2
LWG04 <90389682+LWG04@users.noreply.github.com>
2023-09-28 14:32:26 +0100

    Finished tasks (Lab 1)

--- Tasks/01 - Variables and Arithmetic/121-scanf/121-scanf.cpp
+++ Tasks/01 - Variables and Arithmetic/121-scanf/121-scanf.cpp
@@ -4,18 +4,27 @@ int main()
 {
     //Create a variable x
     int x = 0;
+    int y;
 
     //Prompt user
     printf("Enter your age\n");
 
     //Read keyboard response - look for an integer
-    int found = scanf_s("%d", &x);
+    int found = scanf("%d", &x);
+
+    printf("Enter another number\n");
+
+    int found2 = scanf("%d", &y);
+
+    double average = (x+y)/2;
+
+    printf("The average of the two numbers is: %.2f", average);
 
     //Display how many items were matched
-    printf("Matched %d items\n", found);
+    //printf("Matched %d items\n", found);
 
     //Display what was typed in
-    printf("x = %d\n", x);
+    //printf("x = %d\n", x);
 }
 
 
